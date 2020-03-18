a13ae8da37ae8caaf359bbeb99fc2c466a6da238
Sviat <svyatoslav496@gmail.com>
2020-03-18 14:37:03 +0200

    first point

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+        <div>
+          <img src = "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt = ""></img>
+        </div>
+        <div> Ava + desc</div>
+        <div> My Posts</div>
+        <div>New Post</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>);
+}
+
+export default Profile;
